c9c01821a2f962062c2bdf6bbdff7cd415fa36fb
TranAnh01236 <trananh12a1vvt@gmail.com>
2023-10-31 19:12:15 +0700

    CORS

--- src/main/java/org/trananh/shoppingappbackend/ShoppingAppBackendApplication.java
+++ src/main/java/org/trananh/shoppingappbackend/ShoppingAppBackendApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;	
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;	
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;	
 	
 @EntityScan(basePackages = {"org.trananh.shoppingappbackend.model"})	
 @EnableJpaRepositories(basePackages = {"org.trananh.shoppingappbackend.repository"})
@@ -14,6 +17,16 @@ public class ShoppingAppBackendApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingAppBackendApplication.class, args);	
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 	
 }
